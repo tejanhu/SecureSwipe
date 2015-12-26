ef906776acf4e4c7f37f8ce51bc7d0862a907eee
Hussein Tejan <tejanahmedhu@gmail.com>
2015-12-26 00:16:26 +0000

    Initial commit

---
+++ app/src/main/java/com/example/ec13083/prototype/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ec13083.prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
